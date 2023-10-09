8a89f4d2eb8902a96be464076da13ee73bc48e11
Ramtin Samavat <117670663+RamtinS@users.noreply.github.com>
2023-10-09 19:20:15 +0200

    Add source code and report for Assignment 1

---
+++ Task 6/src/main/java/edu/ntnu/idatt2101/filehandling/FileGraphHandler.java
@@ -0,0 +1,68 @@
+package edu.ntnu.idatt2101.filehandling;
+
+import edu.ntnu.idatt2101.Graph;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * The FileGraphHandler class provides methods for
+ * parsing graphs from files and web resources.
+ *
+ * @author Ramtin Samavat.
+ * @version 1.0
+ * @since Oct 06, 2023.
+ */
+public class FileGraphHandler {
+
+  /**
+   * Parses a graph from a local file.
+   *
+   * @param pathOfFile The path to the local file containing the graph data.
+   * @return The parsed Graph object.
+   * @throws IOException If there is an error reading the file or parsing the graph.
+   */
+  public static Graph parseGraphFromFile(String pathOfFile) throws IOException {
+
+    try (BufferedReader reader = new BufferedReader(
+            new FileReader(pathOfFile.toLowerCase().trim()))) {
+
+      Graph graph = new Graph();
+      graph.newGraph(reader);
+      return graph;
+
+    } catch (IOException e) {
+      String errorMessage = "Error reading graph from file: " + e.getMessage();
+      throw new IOException(errorMessage);
+    }
+  }
+
+  /**
+   * Parses a graph from a web resource.
+   *
+   * @param webURL The URL of the web resource containing the graph data.
+   * @return The parsed Graph object.
+   * @throws IOException If there is an error reading the web resource or parsing the graph.
+   */
+  public static Graph parseGraphFromWebFile(String webURL) throws IOException {
+    try {
+      URL url = new URL(webURL);
+      URLConnection connection = url.openConnection();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+      Graph graph = new Graph();
+      graph.newGraph(reader);
+
+      reader.close();
+
+      return graph;
+    } catch (IOException e) {
+      String errorMessage = "Error reading graph from web: " + e.getMessage();
+      throw new IOException(errorMessage);
+    }
+  }
+}
+
